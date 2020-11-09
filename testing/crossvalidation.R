04b623c8867a699e2ec14dd2b1d7f588e9e7b2ab
AionosChina <schroeder_jan@posteo.de>
2020-11-09 17:20:40 +0100

    working crossvalidation testing script

--- testing/crossvalidation.R
+++ testing/crossvalidation.R
@@ -1,16 +1,20 @@
 
-#PACKAGES
-source("modules/packages.R")
+# CROSSVALIDATION
 
-#SOURCES
+####
+#SOURCES ---------------------------------------------------------------------------
+####
+
+source("modules/packages.R")
 #get password information from yml file
 config = yaml.load_file("modules/config.yml")
-
-#FUNCTIONS
 source("modules/functions.R")
 
-#CONFIGURATIONS
-#--------------------------------------------------------------------------------
+
+####
+#CONFIGURATIONS ---------------------------------------------------------------------------
+####
+
 # database configurations
 con <- dbConnect(RPostgres::Postgres(),
                  dbname = 'nyc_data', 
@@ -18,78 +22,82 @@ con <- dbConnect(RPostgres::Postgres(),
                  port = 5432,
                  user = 'postgres',
                  password = config$TIMESCALEDB$PW)
-#--------------------------------------------------------------------------------
-# time horizon for prediction and train/test split window
-# h = input variable in SHINY
-h <- 48
-#--------------------------------------------------------------------------------
+
 # import configurations
 time_config = "60" #choose input for time_bucket_gapfill function from timescaledb in minutes
 index_config = "pickup_datetime" #choose a table that contains the index
 value_config = "COUNT(*)" #choose the function/table that contains the values
 table_config = "rides" #choose table in database
 
-#DATA
-data <- import_data_from_db(con, time_config, index, value, table)
-
-split_percent <- 0.2
-split_ts_data <- function(data, split_percent){
-  h <- floor(nrow(data)*split_percent)
-  data_train <- head(data, round(length(data) - h))
-  data_test <- tail(data, h)
-  ret.list <- list(data_train, data_test)
-  return(ret.list)
-}
+# percentage to split data
+split_percent <- 0.25
 
 ####
-#ONE ---------------------------------------------------------------------------
+#IMPORT ---------------------------------------------------------------------------
 ####
-#split
-data_train_1 <- split_ts_data(data, split_percent)[[1]] %>% mutate(key = "actual")
-data_test_1 <- split_ts_data(data, split_percent)[[2]] %>% mutate(key = "test")
-#data_1 <- bind_rows(data_train_1, data_test_1)
 
-#to ts
-ts_data.test_1 <- prepare_for_prediction(data_test_1)
-ts_data.train_1 <- prepare_for_prediction(data_train_1)
-
-#predict
-fit_1 <- nnetar(ts_data.train_1)
-fc.fit_1 <- forecast(fit_1, h = nrow(data_test_1), PI = F, npaths = 100)
-
-
-
-#
-# FUNCTION
-#
-
-
-#visual
-list.data.visual_1 <- prepare_for_visual(fc.fit_1, ts_data.train_1, ts_data.test_1)
-ts.extended_1 <- list.data.visual[[2]] 
-df.accuracy_1 <- list.data.visual[[1]] 
+data_1 <- import_data_from_db(con, time_config, index_config, value_config, table_config)
 
 ####
-#TWO ---------------------------------------------------------------------------
+#CROSSVALIDATION ---------------------------------------------------------------------------
 ####
-data_train_2 <- split_ts_data(data_train_1, split_percent)[[1]] %>% mutate(key = "actual")
-data_test_2 <- split_ts_data(data_train_1, split_percent)[[2]] %>% mutate(key = "test")
-#data_2 <- bind_rows(data_train_2, data_test_2)
+
+#CROSSVALIDATION ONE
+tmp <- crossvalidate_data(data_1, split_percent)
+ts_data_1 <- tmp[[1]][[2]][,2]
+df.accuracy_1 <- tmp[[1]][[1]] %>% mutate(Crossvalidation = "3")
+data_2 <- tmp[[2]]
+
+#CROSSVALIDATION TWO
+tmp <- crossvalidate_data(data_2, split_percent)
+ts_data_2 <- tmp[[1]][[2]][,2]
+df.accuracy_2 <- tmp[[1]][[1]] %>% mutate(Crossvalidation = "2")
+data_3 <- tmp[[2]]
+
+#CROSSVALIDATION THREE
+tmp <- crossvalidate_data(data_3, split_percent)
+ts_data_3 <- tmp[[1]][[2]][,2]
+df.accuracy_3 <- tmp[[1]][[1]] %>% mutate(Crossvalidation = "1")
+
+# used later for the visualisation
+data <- prepare_for_prediction(data_1)
+names(data) <- c("Actual")
 
 ####
-#THREE -------------------------------------------------------------------------
+#RESULTS ---------------------------------------------------------------------------
 ####
-data_train_3 <- split_ts_data(data_train_2, split_percent)[[1]] %>% mutate(key = "actual")
-data_test_3 <- split_ts_data(data_train_2, split_percent)[[2]] %>% mutate(key = "test")
-#data_3 <- bind_rows(data_train_3, data_test_3)
 
+#MERGE ACCURACY
+df_accuracy <- bind_rows(df.accuracy_1, df.accuracy_2, df.accuracy_3)
 
+#MERGE RESULTS
+tmp <- merge(ts_data_1, ts_data_2, all = T)
+tmp <- merge(tmp, ts_data_3, all = T)
+ts_data <- merge(tmp, data, all = T)
+names(ts_data) <- c("Prediction_3","Prediction_2","Prediction_1","Actual")
 
-#merge results
-data_merge.tmp <- merge(data_1, data_2, by="index", all=T)
-data_merge <- merge(data_merge.tmp, data_3, by="index", all=T)
-names(data_merge) <- c("index", "value.x",  "key.x", "value.y", "key.y", "value.z", "key.z")
-str(data_merge)
+#check accuracy
+tibble(df_accuracy)
 
+####
+#VISUALIZATION ---------------------------------------------------------------------------
+####
 
+#DETERMIN HIGHLIGHTS IN VISUALIZATION
+df_data <- data.frame(ts_data)
+shade_1.start <- df_data[3] %>% na.omit() %>% head(1) %>% row.names()
+shade_2.start <- df_data[2] %>% na.omit() %>% head(1) %>% row.names()
+shade_3.start <- df_data[1] %>% na.omit() %>% head(1) %>% row.names()
+shade_1.end <- df_data[3] %>% na.omit() %>% tail(1) %>% row.names()
+shade_2.end <- df_data[2] %>% na.omit() %>% tail(1) %>% row.names()
+shade_3.end <- df_data[1] %>% na.omit() %>% tail(1) %>% row.names()
 
+dygraph(ts_data) %>% 
+  dyRangeSelector() %>% 
+  dyOptions(drawPoints = F, pointSize = 2, colors = c("red", "blue","green","black")) %>% 
+  dyUnzoom() %>% 
+  dyCrosshair(direction = "vertical") %>% 
+  dyLegend(width = 400) %>% 
+  dyShading(from = shade_1.start, to = shade_1.end, color = "#CCEBD6") %>% 
+  dyShading(from = shade_2.start, to = shade_2.end, color = "#CCE5FF") %>% 
+  dyShading(from = shade_3.start, to = shade_3.end, color = "#FFE6E6")
